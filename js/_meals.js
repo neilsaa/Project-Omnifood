7d6e906787a45f4b42f96a6ba47cb191201b6669
neilsaa <100754192+neilsaa@users.noreply.github.com>
2023-04-04 23:24:14 +0700

    Add files via upload

---
+++ js/_meals.js
@@ -0,0 +1,547 @@
+const meal1 = {
+  img: "img/meals/meal-1.jpg",
+  categories: ["vegetarian"],
+  name: "Japanese Gyozas",
+  description:
+    "This super easy n’oatmeal is made in just 3 minutes and it’s gluten, paleo, vegan",
+  Calories: 650,
+  NutriScore: 74,
+  Rating: 4.9,
+  numRate: 537,
+};
+
+const meal2 = {
+  img: "img/meals/meal-2.jpg",
+  categories: ["vegetarian", "paleo"],
+  name: "JAvocado Salad",
+  description:
+    "This super easy n’oatmeal is made in just 3 minutes and it’s gluten, paleo, vegan",
+  Calories: 400,
+  NutriScore: 92,
+  Rating: 4.8,
+  numRate: 441,
+};
+
+const PVG_meals = {
+  img: "img/meals/meal-3.jpg",
+  categories: ["paleo", "vegan", "gluten"],
+  name: "Grain Cereal",
+  description:
+    "This super easy n’oatmeal is made in just 3 minutes and it’s gluten, paleo, vegan",
+  Calories: 400,
+  NutriScore: 74,
+  Rating: 4.8,
+  numRate: 430,
+};
+const PVG_meals_1 = {
+  img: "img/meals/meal-4.jpg",
+  categories: ["paleo", "vegan", "gluten"],
+  name: "Quinoa Bowl",
+  description:
+    "Full of fruits, nuts, and with gluten free oats, this is not only nourishing but also delicious",
+  Calories: 390,
+  NutriScore: 75,
+  Rating: 4.5,
+  numRate: 420,
+};
+const PVG_meals_2 = {
+  img: "img/meals/meal-5.jpg",
+  categories: ["paleo", "vegan", "gluten"],
+  name: "Cauliflower Oatmeal",
+  description:
+    "Combo of rolled oats and cauliflower rice makes an easy way to sneak in some veggies, fiber, and micronutrients into a comforting bowl of oatmeal",
+  Calories: 410,
+  NutriScore: 96,
+  Rating: 4.8,
+  numRate: 530,
+};
+const PVG_meals_3 = {
+  img: "img/meals/meal-6.jpg",
+  categories: ["paleo", "vegan", "gluten"],
+  name: "Avocado Salad",
+  description:
+    "Healthy green salad with fresh avocado, spinach, tomatoes and citrus lime dressing",
+  Calories: 400,
+  NutriScore: 92,
+  Rating: 4.8,
+  numRate: 472,
+};
+const PVG_meals_4 = {
+  img: "img/meals/meal-7.jpg",
+  categories: ["paleo", "vegan", "gluten"],
+  name: "Fluffy Sweet Potato",
+  description:
+    "What do you eat when it’s too cold for a smoothie, pancakes sound too complicated, and you’ve had oats for breakfast for way too many days in a row? Fluffy Sweet Potato Breakfast Bowls, of course!",
+  Calories: 380,
+  NutriScore: 92,
+  Rating: 4.7,
+  numRate: 512,
+};
+const LVK = {
+  img: "img/meals/meal-8.jpg",
+  categories: ["gluten", "vegan", "keto"],
+  name: "Japanese Gyozas",
+  description:
+    "Juicy pork, cabbage, scallions, and shiitake mushrooms with a rich Ponzu sauce drizzle",
+  Calories: 420,
+  NutriScore: 74,
+  Rating: 4.6,
+  numRate: 430,
+};
+const LVK1 = {
+  img: "img/meals/meal-9.jpg",
+  categories: ["gluten", "vegan", "keto"],
+  name: "Bacon And Eggs",
+  description:
+    "Among other pork products, bacon is high in fat, making it the ideal candidate for a keto-friendly, low carb breakfast",
+  Calories: 410,
+  NutriScore: 80,
+  Rating: 4.8,
+  numRate: 414,
+};
+const LVK2 = {
+  img: "img/meals/meal-10.jpg",
+  categories: ["gluten", "vegan", "keto"],
+  name: "Salmon With Tomatoes",
+  description:
+    "A foolproof trio loaded with flavor: flavorful salmon dressed up with a juicy olive and crunchy pistachio tapenade",
+  Calories: 450,
+  NutriScore: 80,
+  Rating: 4.9,
+  numRate: 532,
+};
+const LVK3 = {
+  img: "img/meals/meal-11.jpg",
+  categories: ["gluten", "vegan", "keto"],
+  name: "Zucchini Salad",
+  description:
+    "Zucchini stands in for potatoes in this low-carb makeover of the classic summer fave, potato salad. All of the creamy flavor, and (almost) none of the carbs! Perfect for picnics and barbecues",
+  Calories: 360,
+  NutriScore: 60,
+  Rating: 4.8,
+  numRate: 600,
+};
+const LVK4 = {
+  img: "img/meals/meal-12.jpg",
+  categories: ["gluten", "vegan", "keto"],
+  name: "Banana Waffles",
+  description:
+    "Love waffles? Adore bananas? Waffles + bananas = perfection! Why not try these delicious, dairy-free waffles for dessert",
+  Calories: 350,
+  NutriScore: 50,
+  Rating: 4.8,
+  numRate: 430,
+};
+const VK = {
+  img: "img/meals/meal-13.jpg",
+  categories: ["vegetarian", "kid", "pesca"],
+  name: "Rainbow Kebabs",
+  description:
+    "A fun snack for kids that'll really get 'em eating more veggies! Pair with your fave vegan dip for a super-nutritious rainbow at snack time!",
+  Calories: 300,
+  NutriScore: 50,
+  Rating: 4.6,
+  numRate: 430,
+};
+const VK1 = {
+  img: "img/meals/meal-14.jpg",
+  categories: ["vegetarian", "kid", "pesca"],
+  name: "Pizza With Pesto",
+  description:
+    "Your kids will love these vegan Pita Pizza topped with lots of vegetables, sweet caramelized onions, and a pesto even more vegetables. Quick, easy, and gluten!",
+  Calories: 400,
+  NutriScore: 70,
+  Rating: 4.8,
+  numRate: 430,
+};
+const VK2 = {
+  img: "img/meals/meal-15.jpg",
+  categories: ["vegetarian", "kid"],
+  name: "Pasta Salad",
+  description:
+    "Fun fusilli pasta, creamy vegan pesto, earthy chickpeas, and bright roasted vegetables, combine for a quick, easy, and colorful vegan pesto pasta salad",
+  Calories: 350,
+  NutriScore: 55,
+  Rating: 4.7,
+  numRate: 310,
+};
+const VK3 = {
+  img: "img/meals/meal-16.jpg",
+  categories: ["vegetarian", "kid", "pesca"],
+  name: "Vegan Tacos",
+  description:
+    "Kids love tacos--why not serve them for breakfast, too? These vegan Breakfast Tacos are a savory take on breakfast with a Mexican-inspired twist. They’re easy to make, gluten, and best of all – delicious!",
+  Calories: 330,
+  NutriScore: 50,
+  Rating: 4.7,
+  numRate: 300,
+};
+const VK4 = {
+  img: "img/meals/meal-17.jpg",
+  categories: ["vegetarian", "kid", "pesca"],
+  name: "Zucchini Muffins",
+  description:
+    "These vegan zucchini muffins are soft, sweet, and full of healthy ingredients. With no oil or refined sugar in the batter, you can feel good about serving them to your family",
+  Calories: 310,
+  NutriScore: 50,
+  Rating: 4.8,
+  numRate: 567,
+};
+
+const PKL = {
+  img: "img/meals/meal-18.jpg",
+  categories: ["paleo", "keto", "fodmap"],
+  name: "Chicken Caesar Cobb",
+  description:
+    "Grilled chicken, fresh avocado with veggie toppings and creamy caesar dressing",
+  Calories: 410,
+  NutriScore: 93,
+  Rating: 4.9,
+  numRate: 417,
+};
+
+const PKL1 = {
+  img: "img/meals/meal-19.jpg",
+  categories: ["paleo", "keto", "fodmap"],
+  name: "Chicken Breast",
+  description:
+    "This pub-inspired seasoned chicken breast is a fast family hit. Its simple flavour makes it perfect for a salad or sandwich, as well as the star of a heavier meal",
+  Calories: 400,
+  NutriScore: 90,
+  Rating: 4.7,
+  numRate: 479,
+};
+
+const PKL2 = {
+  img: "img/meals/meal-20.jpg",
+  categories: ["paleo", "keto", "fodmap"],
+  name: "Marinated Chicken",
+  description: "Marinated chicken is wonderful to make for busy nights",
+  Calories: 350,
+  NutriScore: 80,
+  Rating: 4.8,
+  numRate: 330,
+};
+
+const PKL3 = {
+  img: "img/meals/meal-21.jpg",
+  categories: ["paleo", "keto", "fodmap"],
+  name: "Chicken And Bean",
+  description:
+    "One-skillet chicken dish that can be on the table in just 30 minutes, so it is perfect for those busy weeknights when you’re tempted to grab some takeout",
+  Calories: 360,
+  NutriScore: 83,
+  Rating: 4.9,
+  numRate: 430,
+};
+
+const PKL4 = {
+  img: "img/meals/meal-22.jpg",
+  categories: ["paleo", "keto", "fodmap"],
+  name: "Pollo Asado",
+  description:
+    "This hearty roast chicken dinner is packed with comforting flavors, which is highlighted in its delicious mix of rice and beans. Plus, the entire dish is also done in under an hour! Ingredients",
+  Calories: 380,
+  NutriScore: 87,
+  Rating: 4.6,
+  numRate: 430,
+};
+
+const paleo0 = {
+  img: "img/meals/meal-23.jpg",
+  categories: ["paleo"],
+  name: "Grilled Chicken",
+  description:
+    "A refreshing and nutritious go-to summer salad with mixed greens, toppings and grilled chicken with balsamic dressing",
+  Calories: 435,
+  NutriScore: 90,
+  Rating: 4.7,
+  numRate: 730,
+};
+
+const paleo1 = {
+  img: "img/meals/meal-24.jpg",
+  categories: ["paleo"],
+  name: "Rotisserie Chicken",
+  description:
+    "A hearty, healthy meal that’s halfway done before you even start cooking",
+  Calories: 425,
+  NutriScore: 89,
+  Rating: 4.9,
+  numRate: 630,
+};
+
+const paleo2 = {
+  img: "img/meals/meal-25.jpg",
+  categories: ["paleo"],
+  name: "Spring Salad",
+  description:
+    "This easy and beautiful spring mix salad features tender baby greens, crunchy radishes, and nutty sunflower seeds lightly tossed in a homemade honey lemon vinaigrette",
+  Calories: 350,
+  NutriScore: 78,
+  Rating: 4.6,
+  numRate: 450,
+};
+
+const vegan0 = {
+  img: "img/meals/meal-26.jpg",
+  categories: ["vegan"],
+  name: "Vegan Tacos",
+  description:
+    "These tacos are brilliant for entertaining: simply serve the warmed tortillas with the veg and beans, pickled onions, salsa, avocado and yogurt alongside, and let everyone build their own",
+  Calories: 340,
+  NutriScore: 75,
+  Rating: 4.8,
+  numRate: 431,
+};
+
+const vegan1 = {
+  img: "img/meals/meal-27.jpg",
+  categories: ["vegan"],
+  name: "Vegan Ramen",
+  description:
+    "Recreate this deliciously creamy, spicy, slurpable bowl of vegan ramen for a meal that will leave you feeling full and nourished.",
+  Calories: 350,
+  NutriScore: 78,
+  Rating: 4.6,
+  numRate: 436,
+};
+
+const vegan2 = {
+  img: "img/meals/meal-28.jpg",
+  categories: ["vegan"],
+  name: "Squash Pie",
+  description:
+    "Our spiced butternut pie with a vegan pie crust is a brilliant dinner party option and it's just as good eaten the next day – leftovers make a great lunch with a dollop of mango chutney or extra harissa swirled into coconut yogurt.",
+  Calories: 355,
+  NutriScore: 82,
+  Rating: 4.7,
+  numRate: 346,
+};
+
+const keto0 = {
+  img: "img/meals/meal-29.jpg",
+  categories: ["keto"],
+  name: "Chicken With Broccoli",
+  description:
+    "So simple and enjoyable! keto fried chicken and broccoli is a perfect example of how to create a keto plate by combining a protein, a vegetable, and fat for flavor.",
+  Calories: 380,
+  NutriScore: 86,
+  Rating: 4.9,
+  numRate: 337,
+};
+
+const keto1 = {
+  img: "img/meals/meal-30.jpg",
+  categories: ["keto"],
+  name: "Jalapeño Chaffles",
+  description:
+    "Chaffles are the go-to keto craze that can rock any flavor, sweet or savory. In this family-friendly recipe, chaffles get a spicy upgrade with layers of cream cheese, bacon, and jalapeño poppers. Best of all, they’re fun to make and to eat!",
+  Calories: 370,
+  NutriScore: 84,
+  Rating: 4.8,
+  numRate: 430,
+};
+
+const keto2 = {
+  img: "img/meals/meal-31.jpg",
+  categories: ["keto"],
+  name: "Seared Salmon",
+  description:
+    "This tender salmon recipe (with a creamy lemon sauce) incorporates everything we love about keto. Lots of flavor, simple ingredients, and butter — with none of the carbs.",
+  Calories: 380,
+  NutriScore: 85,
+  Rating: 4.9,
+  numRate: 430,
+};
+const Glutenfree = {
+  img: "img/meals/meal-32.jpg",
+  categories: ["gluten"],
+  name: "Yorkshire Puddings",
+  description:
+    "The perfect vehicle for gravy and roasted goodies: the Yorkshire pudding - this version is gluten but never fear, you still get that characteristic puff and golden colour",
+  Calories: 350,
+  NutriScore: 70,
+  Rating: 4.8,
+  numRate: 430,
+};
+
+const kidfriendly = {
+  img: "img/meals/meal-33.jpg",
+  categories: ["kid"],
+  name: "Sesame Chicken",
+  description:
+    "In this quick and easy recipe, tender bites of chicken are coated in a sticky sweet-and-savory sauce. My whole family devoured this chicken - it's that good!",
+  Calories: 360,
+  NutriScore: 76,
+  Rating: 4.8,
+  numRate: 430,
+};
+
+const kidfriendly1 = {
+  img: "img/meals/meal-34.jpg",
+  categories: ["kid"],
+  name: "Baked Chicken",
+  description:
+    "No list of kid recipes would be complete without chicken tenders! These bake up crispy on the outside and juicy on the inside. Serve them with a few dipping sauces and let everyone choose their favorite.",
+  Calories: 360,
+  NutriScore: 76,
+  Rating: 4.8,
+  numRate: 429,
+};
+
+const arr = [
+  meal1,
+  meal2,
+  PVG_meals,
+  PVG_meals_1,
+  PVG_meals_2,
+  PVG_meals_3,
+  PVG_meals_4,
+  LVK,
+  LVK1,
+  LVK2,
+  LVK3,
+  LVK4,
+  VK,
+  VK1,
+  VK2,
+  VK3,
+  VK4,
+  PKL,
+  PKL1,
+  PKL2,
+  PKL3,
+  PKL4,
+  paleo0,
+  paleo1,
+  paleo2,
+  vegan0,
+  vegan1,
+  vegan2,
+  keto0,
+  keto1,
+  keto2,
+  Glutenfree,
+  kidfriendly,
+  kidfriendly1,
+];
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// RENDER CATEGORY
+const categoriesRender = function (arr) {
+  let out = "";
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    out += `<span class="tag tag--${arr[i]}">${arr[i]}</span>`;
+  }
+
+  return out;
+};
+
+// console.log(categoriesRender(PVG_meals.categories));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// RENDER MEALS NAME
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// RENDER MEAL CARDS
+const resetMeal = function () {
+  mealSlider.innerHTML = "";
+};
+const renderMeal = function (m) {
+  mealSlider.innerHTML += `<div class="meal">
+  <img
+    src="${m.img}"
+    alt="Japanese Gyozas"
+    class="meal-img"
+  />
+
+  <div class="meal-content">
+    <div class="meal-tags">
+   ${categoriesRender(m.categories)}
+    </div>
+
+    <p class="meal-title">${m.name}</p>
+    <ul class="meal-attributes">
+      <li class="meal-attribute">
+        <ion-icon class="meal-icon" name="flame-outline"></ion-icon>
+        <span> <strong>${m.Calories}</strong> calories</span>
+      </li>
+      <li class="meal-attribute">
+        <ion-icon
+          class="meal-icon"
+          name="restaurant-outline"
+        ></ion-icon>
+        <span>NutriScore &reg; <strong>${m.NutriScore}</strong></span>
+      </li>
+      <li class="meal-attribute">
+        <ion-icon class="meal-icon" name="star-outline"></ion-icon
+        ><span> <strong>${m.Rating}</strong> rating (${m.numRate})</span>
+      </li>
+    </ul>
+  </div>
+</div>`;
+};
+const mealSlider = document.querySelector(".meals-slide");
+
+for (const m of arr) {
+  renderMeal(m);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// FILTER THE MEALS
+// const mealBtns = document.querySelectorAll(".meals-types-overview .tag");
+// console.log(mealBtns);
+
+// const mealBtnsContainer = document.querySelector(
+//   ".meals-types-overview .meal-types-list"
+// );
+// let activeType = "";
+// mealBtnsContainer.addEventListener("click", function (e) {
+//   console.log(e.target);
+//   if (!e.target.classList.contains("tag")) return;
+//   resetMeal();
+//   if (e.target.classList.contains("tag-active")) {
+//     mealBtns.forEach((element) => {
+//       element.classList.remove("tag-active");
+//       console.log("Run");
+//     });
+//     for (const m of arr) {
+//       renderMeal(m);
+//     }
+//   } else {
+//     e.target.classList.add("tag-active");
+//     for (const m of arr) {
+//       if (m.categories.includes(e.target.getAttribute("name"))) renderMeal(m);
+//     }
+//   }
+// });
+{
+  /* <div class="meal">
+            <img
+              src="img/meals/meal-1.jpg"
+              alt="Japanese Gyozas"
+              class="meal-img"
+            />
+
+            <div class="meal-content">
+              <div class="meal-tags">
+                <span class="tag tag--vegetarian">Vegetarian</span>
+              </div>
+
+              <p class="meal-title">Japanese Gyozas</p>
+              <ul class="meal-attributes">
+                <li class="meal-attribute">
+                  <ion-icon class="meal-icon" name="flame-outline"></ion-icon>
+                  <span> <strong>650</strong> calories</span>
+                </li>
+                <li class="meal-attribute">
+                  <ion-icon
+                    class="meal-icon"
+                    name="restaurant-outline"
+                  ></ion-icon>
+                  <span>NutriScore &reg; <strong>74</strong></span>
+                </li>
+                <li class="meal-attribute">
+                  <ion-icon class="meal-icon" name="star-outline"></ion-icon
+                  ><span> <strong>4.9</strong> rating (537)</span>
+                </li>
+              </ul>
+            </div>
+          </div> */
+}
---
+++ js/script.js
@@ -0,0 +1,216 @@
+// Set current year
+const yearEl = document.querySelector(".year");
+console.log(yearEl);
+const currentYear = new Date().getFullYear();
+// console.log(currentYear);
+yearEl.textContent = currentYear;
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function (e) {
+  headerEl.classList.toggle("nav-open");
+});
+
+////////////////////////////////////////// Smooth scrooling animation
+
+const allLinks = document.querySelectorAll("a:link");
+// console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    const href = link.getAttribute("href");
+    // Scrool back to the top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // Close mobile navigation
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.remove("nav-open");
+    }
+  });
+});
+
+////////////////////////////////////////// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    // console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
+// ////////////////////////////////////////////////////////////////////// Screenshot animation
+
+const stepImgBox = document.querySelectorAll(".step-img-box");
+// console.log(stepImgBox);
+
+// const observer2 = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
+//     if (ent.isIntersecting) {
+//       stepImgBox[0].classList.add("animate-img");
+//     } else {
+//       stepImgBox[0].classList.remove("animate-img");
+//     }
+//   },
+//   {
+//     root: null,
+//     threshold: 0,
+//   }
+// );
+// observer2.observe(stepImgBox[0]);
+stepImgBox.forEach(function (el) {
+  const observer = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+      // console.log(ent);
+      if (ent.isIntersecting) {
+        el.classList.add("animate-img");
+      } else {
+        el.classList.remove("animate-img");
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+    }
+  );
+  observer.observe(el);
+});
+///////////////////////////////////////////////////////////////////////////// PRICING OPTION CHANGE
+
+const pricingOptions = document.querySelector(".pricing-options");
+const pricingOption = document.querySelectorAll(".pricing-option");
+const planPrice = document.querySelectorAll(".plan-price");
+// const planText = document.querySelectorAll(".plan-text");/
+let currentActivePricing = 0;
+const priceArr = [
+  [345, 345 * 11],
+  [649, 649 * 11],
+];
+console.log(pricingOption);
+pricingOptions.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target.getAttribute("class") === "pricing-options") {
+    pricingOption.forEach(function (el) {
+      el.classList.toggle("pricing-option-active");
+      currentActivePricing = !currentActivePricing;
+    });
+  } else if (e.target.getAttribute("name") === "pricing-year") {
+    pricingOption[0].classList.remove("pricing-option-active");
+    pricingOption[1].classList.add("pricing-option-active");
+    currentActivePricing = 1;
+  } else {
+    pricingOption[1].classList.remove("pricing-option-active");
+    pricingOption[0].classList.add("pricing-option-active");
+    currentActivePricing = 0;
+  }
+
+  for (let index = 0; index < planPrice.length; index++) {
+    const element = planPrice[index];
+    element.innerHTML = `<span>$</span>${priceArr[index][currentActivePricing]}`;
+  }
+});
+
+//////////////////////////////////////////////////////////////////// Pagination
+///////////////////////////////////////////////////////////
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/*
+.no-flexbox-gap .main-nav-list li:not(:last-child) {
+  margin-right: 4.8rem;
+}
+
+.no-flexbox-gap .list-item:not(:last-child) {
+  margin-bottom: 1.6rem;
+}
+
+.no-flexbox-gap .list-icon:not(:last-child) {
+  margin-right: 1.6rem;
+}
+
+.no-flexbox-gap .delivered-meal {
+  margin-right: 1.6rem;
+}
+
+.no-flexbox-gap .meal-attribute:not(:last-child) {
+  margin-bottom: 2rem;
+}
+
+.no-flexbox-gap .meal-icon {
+  margin-right: 1.6rem;
+}
+
+.no-flexbox-gap .footer-row div:not(:last-child) {
+  margin-right: 6.4rem;
+}
+
+.no-flexbox-gap .social-links li:not(:last-child) {
+  margin-right: 2.4rem;
+}
+
+.no-flexbox-gap .footer-nav li:not(:last-child) {
+  margin-bottom: 2.4rem;
+}
+
+@media (max-width: 75em) {
+  .no-flexbox-gap .main-nav-list li:not(:last-child) {
+    margin-right: 3.2rem;
+  }
+}
+
+@media (max-width: 59em) {
+  .no-flexbox-gap .main-nav-list li:not(:last-child) {
+    margin-right: 0;
+    margin-bottom: 4.8rem;
+  }
+}
+*/
